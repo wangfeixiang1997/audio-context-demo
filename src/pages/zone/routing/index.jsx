c670a45421b9bdc0b9b21e51bdb1c60c7433d830
wangfeixiang <wangfeixiang@shgbit.com>
2023-12-14 14:08:48 +0800

    fix: 修改音量条移入和点击效果，routing部分页面

--- src/pages/zone/routing/index.jsx
+++ src/pages/zone/routing/index.jsx
@@ -1,7 +1,10 @@
 import { useEffect } from 'react'
+import {
+    NodeCollapseOutlined,
+} from '@ant-design/icons';
+import './index.css';
 
 const Routing = () => {
-
 
     useEffect(() => {
 
@@ -9,7 +12,17 @@ const Routing = () => {
 
 
     return (
-        <div style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>开发中...</div>
+        <div>
+            <div className='routing-item-box'>
+                <div className='routing-item-title'>
+                    <span><NodeCollapseOutlined /></span>
+                    <span>Routing</span>
+                </div>
+                <div className='routing-item-content'>
+                    <div>Select Zone Signal source for Speaker</div>
+                </div>
+            </div>
+        </div>
     )
 }
 
